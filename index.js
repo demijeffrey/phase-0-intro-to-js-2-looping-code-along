e49532003d7f86ed1e1d38f2bb726d86240042b7
Demaris Ashmore <demiashmore@yahoo.com>
2022-07-23 13:04:57 -0600

    commit tree 208e9792fd686042e79b3b8a2ffe4afa13fca7c4

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ['Jeff', 'Demi', 'Toby', 'Maze']
+const event = "birthday"
+
+function writeCards(names, event) {
+    let result = []
+  for (let a = 0; a < names.length; a++) {
+    result.push(`Thank you, ${names[a]}, for the wonderful ${event} gift!`)
+  }
+  return result
+}
+writeCards(['Jeff', 'Demi', 'Toby', 'Maze'], 'birthday')
+
+
+function countDown(b) {
+    let countDown = b
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+}
+countDown(15)
